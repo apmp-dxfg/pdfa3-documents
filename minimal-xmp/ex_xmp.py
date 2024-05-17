5764f72995b143b29ec06e664c5101da0d2000b1
Blair Hall <blairdhall@gmail.com>
2024-05-17 16:51:07 +1200

    This is working

--- minimal-xmp/ex_xmp.py
+++ minimal-xmp/ex_xmp.py
@@ -2,7 +2,8 @@ from pypdf import PdfReader
 
 meta = PdfReader("ex_xmp.pdf").xmp_metadata 
 
-print( meta.dc_title )
-print( meta.dc_creator )
-print( meta.dc_date )
-print( meta.dc_publisher )
+print( meta.dc_title['x-default'] )
+print( ",".join( meta.dc_creator ) )
+        
+
+
